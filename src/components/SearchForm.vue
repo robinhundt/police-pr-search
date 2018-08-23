c39ce5c23ed5dc132b9b30379dc4a563060f2e96
robinwilliam.hundt <robinwilliam.hundt@stud.uni-goettingen.de>
2018-08-23 21:36:15 +0200

    Started data preparation and basic search website (non functional)

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,90 @@
+<template>
+    <v-form>
+        <v-text-field
+                v-model="title"
+                label="Title"
+        />
+        <v-textarea
+                v-model="body"
+                label="(Partial) body of the article"
+        />
+        <v-text-field
+                v-model="location"
+                label="Location"
+        />
+        <v-layout row>
+
+            <v-menu
+                    ref="menuFrom"
+                    :close-on-content-click="false"
+                    v-model="menuFrom"
+                    :nudge-right="40"
+                    :return-value.sync="dateFrom"
+                    lazy
+                    transition="scale-transition"
+                    offset-y
+                    full-width
+                    min-width="290px"
+            >
+                <v-text-field
+                        slot="activator"
+                        v-model="dateFrom"
+                        label="From Date"
+                        prepend-icon="event"
+                        readonly
+                ></v-text-field>
+                <v-date-picker v-model="dateFrom" no-title scrollable>
+                    <v-spacer></v-spacer>
+                    <v-btn flat color="primary" @click="menuFrom = false">Cancel</v-btn>
+                    <v-btn flat color="primary" @click="$refs.menuFrom.save(dateFrom)">OK</v-btn>
+                </v-date-picker>
+            </v-menu>
+            <v-menu
+                    ref="menuTo"
+                    :close-on-content-click="false"
+                    v-model="menuTo"
+                    :nudge-right="40"
+                    :return-value.sync="dateTo"
+                    lazy
+                    transition="scale-transition"
+                    offset-y
+                    full-width
+                    min-width="290px"
+            >
+                <v-text-field
+                        slot="activator"
+                        v-model="dateTo"
+                        label="To Date"
+                        prepend-icon="event"
+                        readonly
+                ></v-text-field>
+                <v-date-picker v-model="dateTo" no-title scrollable>
+                    <v-spacer></v-spacer>
+                    <v-btn flat color="primary" @click="menuTo = false">Cancel</v-btn>
+                    <v-btn flat color="primary" @click="$refs.menuTo.save(dateTo)">OK</v-btn>
+                </v-date-picker>
+            </v-menu>
+        </v-layout>
+    </v-form>
+</template>
+
+<script>
+export default {
+  name: 'SearchForm',
+  data () {
+    return {
+      menuFrom: false,
+      menuTo: false,
+      title: '',
+      body: '',
+      location: '',
+      dateFrom: undefined,
+      dateTo: undefined
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  <v-container fluid>
+    <v-flex class="md4 offset-md4">
+      <h1 class="text-xs-center">Police Press Release Search</h1>
+      <search-form/>
+    </v-flex>
+  </v-container>
 </template>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SearchForm from '../components/SearchForm'
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  components: {SearchForm}
 }
 </script>
